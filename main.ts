35360cf99cef317407cbbb8e6edb5f36fd6da94b
jntroye <143399107+jntroye@users.noreply.github.com>
2023-08-28 14:10:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-let Strip_1: neopixel.Strip = null
-let StepLength = 0
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . . .
@@ -8,23 +6,23 @@ input.onButtonPressed(Button.A, function () {
         # . # . .
         . # . . .
         `)
-    Strip_1.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    Strip_1.show()
-    basic.pause(StepLength)
-    Strip_1.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-    Strip_1.show()
-    basic.pause(StepLength)
-    Strip_1.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    Strip_1.show()
-    basic.pause(StepLength)
-    Strip_1.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-    basic.pause(StepLength)
-    Strip_1.show()
-    Strip_1.setPixelColor(4, neopixel.colors(NeoPixelColors.Green))
-    basic.pause(StepLength)
-    Strip_1.show()
-    basic.pause(StepLength)
-    Strip_1.clear()
+    Strip_12.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    Strip_12.show()
+    basic.pause(myStepLength)
+    Strip_12.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    Strip_12.show()
+    basic.pause(myStepLength)
+    Strip_12.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    Strip_12.show()
+    basic.pause(myStepLength)
+    Strip_12.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    basic.pause(myStepLength)
+    Strip_12.show()
+    Strip_12.setPixelColor(4, neopixel.colors(NeoPixelColors.Green))
+    basic.pause(myStepLength)
+    Strip_12.show()
+    basic.pause(myStepLength)
+    Strip_12.clear()
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
@@ -35,23 +33,29 @@ input.onButtonPressed(Button.B, function () {
         . # . # .
         # . . . #
         `)
-    Strip_1.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(StepLength)
-    Strip_1.clear()
-    basic.pause(StepLength)
-    Strip_1.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(StepLength)
-    Strip_1.clear()
-    basic.pause(StepLength)
-    Strip_1.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(StepLength)
-    Strip_1.clear()
-    basic.pause(StepLength)
-    Strip_1.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(StepLength)
-    Strip_1.clear()
+    Strip_12.showColor(neopixel.colors(NeoPixelColors.Red))
+    Strip_12.clear()
+    basic.pause(myStepLength)
+    Strip_12.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(myStepLength)
+    Strip_12.clear()
+    basic.pause(myStepLength)
+    Strip_12.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(myStepLength)
+    Strip_12.clear()
+    basic.pause(myStepLength)
+    Strip_12.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(myStepLength)
+    Strip_12.clear()
     basic.clearScreen()
+    basic.pause(myStepLength)
 })
+let Strip_12: neopixel.Strip = null
+let myStepLength = 0
+myStepLength = 0
+// let Strip_1: neopixel.Strip = null
+Strip_12 = neopixel.create(DigitalPin.P5, 5, NeoPixelMode.RGB)
+myStepLength = 50
 basic.forever(function () {
 	
 })
